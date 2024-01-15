b545ac3fb864db0069b267f4654f4198200141b7
buyung1610 <123521836+buyung1610@users.noreply.github.com>
2024-01-15 17:32:02 +0700

    flutter_drawer
    flutter_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import './page/page_2.dart';
-import './page/page_3.dart';
-import './page/page_1.dart';
+import 'page/settings.dart';
+import 'page/about.dart';
 
 void main() {
   runApp(myApp());
@@ -10,14 +9,78 @@ void main() {
 class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/page1',
+      home: myHomePage(),
       routes: {
-        '/page1' : (context) => page1(),
-        '/page2' : (context) => page2(),
-        '/page3' : (context) => page3()
+        '/page1' : (context) => about(),
+        '/page2' : (context) => settings(),
       },
+    );
+  }
+}
+
+class myHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Drawer"),
+      ),
+      body: const Center(
+        child: Text(
+          "HOME PAGE",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              height: 150,
+              color: Colors.amber,
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Menu Pilihan",
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushNamed('/page1');
+              },
+              leading: Icon(
+                Icons.info,
+                size: 25,
+              ),
+              title: Text(
+                'About',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushNamed('/page2');
+              },
+              leading: Icon(
+                Icons.settings,
+                size: 25,
+              ),
+              title: Text(
+                'Settings',
+                style: TextStyle(fontSize: 15),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
